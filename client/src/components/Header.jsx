163f48f076eb923dbb1353d5b3f393dcbcb8a826
ashutosh sharma <ashusharmads.18@gmail.com>
2024-08-21 16:44:24 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
     <Link to="/" className='self-center whitespace-nowrap text-sm 
     sm:text-xl font-semibold dark:text-white'>
     <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
-    via-purple-500 to-pink-500 rounded-lg text-white'>Sahand'S</span>
+    via-purple-500 to-pink-500 rounded-lg text-white'>SADANS'S</span>
     Blog
     </Link>
     <form>
@@ -31,7 +31,7 @@ function Header() {
             <FaMoon/>
         </Button>
       <Link to='/sign-in'>
-      <Button gradientDuoTone='purpleToBlue'>
+      <Button gradientDuoTone='purpleToBlue'outline>
         Sign In
       </Button>
       </Link>
